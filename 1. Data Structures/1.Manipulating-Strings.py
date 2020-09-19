2c9a57838188ec8c90e70f27b8329d1937a96b84
kareem ameen <kareem.aameen@gmail.com>
2020-09-19 16:20:49 -0500

    Update 1.Manipulating-Strings.py

--- 1. Data Structures/1.Manipulating-Strings.py
+++ 1. Data Structures/1.Manipulating-Strings.py
@@ -25,3 +25,35 @@ print(position)
 
 r = fruit.replace("ree" , "nnn")
 print (r)
+
+#how to strip white spaces from a test
+#a str class library can be used for that. the library name is strip
+greet = "    Hello Kareem    "
+print(greet)
+x = greet.lstrip() #strips left spaces
+print(x)
+x = greet.rstrip() #strips right spaces
+print(x)
+x = greet.strip() #strips all white spaces
+print(x)
+
+print("              ")
+print("              ")
+
+#find a prefix
+line = "please have a nice day"
+answer = line.startswith("please") #asks if line starts with "please"
+print(answer)
+answer = line.startswith("P") #asks if line starts with capital P
+print(answer)
+
+print("              ")
+print("              ")
+
+#slicing data
+#we need to get the host of a sent email
+data = "From kareem.aameen@gmail.com Sat Jan 5 09:14:16 2008"
+#find where the "@" is in data
+atpos = data.find("@")
+#find where a space comes after the first @
+sppos = data.
